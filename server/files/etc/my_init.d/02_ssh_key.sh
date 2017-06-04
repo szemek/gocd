f8bf954ff5510a634667e328eaf310f92ea8ba67
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2017-06-05 00:34:59 +0200

    copy private key from mounted volume

--- server/files/etc/my_init.d/02_ssh_key.sh
+++ server/files/etc/my_init.d/02_ssh_key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 mkdir -p /var/go/.ssh
-echo "${SSH_ID_RSA}" > /var/go/.ssh/id_rsa
+cp /etc/secrets/id_rsa /var/go/.ssh/id_rsa
 chown -R go:go /var/go/.ssh
 chmod 0400 /var/go/.ssh/id_rsa
